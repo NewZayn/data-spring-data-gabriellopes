4c24a6cf69e894bb493294f2a7702e86935fe445
Gabriel Lopes <gabrielseverino.lopesguimares@jala.university>
2024-09-23 17:06:06 -0300

    projeto

---
+++ demo-spring-data-Gabriel-Lopes/src/main/java/com/example/demo_spring_data_Gabriel_Lopes/DemoSpringDataGabrielLopesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_data_Gabriel_Lopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDataGabrielLopesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringDataGabrielLopesApplication.class, args);
+	}
+
+}
---
+++ demo-spring-data-Gabriel-Lopes/src/test/java/com/example/demo_spring_data_Gabriel_Lopes/DemoSpringDataGabrielLopesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_data_Gabriel_Lopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDataGabrielLopesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
