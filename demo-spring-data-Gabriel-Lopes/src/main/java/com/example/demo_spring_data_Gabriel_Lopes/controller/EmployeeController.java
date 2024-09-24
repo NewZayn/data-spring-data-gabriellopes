6c160ebc1c843f411cd5135f8e3eae12f79ec7d3
Gabriel Lopes <gabrielseverino.lopesguimares@jala.university>
2024-09-23 23:59:27 -0300

    ajuste no arquivo Docker

--- demo-spring-data-Gabriel-Lopes/src/main/java/com/example/demo_spring_data_Gabriel_Lopes/controller/EmployeeController.java
+++ demo-spring-data-Gabriel-Lopes/src/main/java/com/example/demo_spring_data_Gabriel_Lopes/controller/EmployeeController.java
@@ -1,25 +1,26 @@
 package com.example.demo_spring_data_Gabriel_Lopes.controller;
 
-
 import com.example.demo_spring_data_Gabriel_Lopes.jpa.Employee;
 import com.example.demo_spring_data_Gabriel_Lopes.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
-     EmployeeService employeeService;
+    @Autowired
+    private EmployeeService employeeService;
 
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
 
-     @PostMapping("/employee")
-     public Employee employee(@RequestBody Employee employee) {
-         return employeeService.addEmployee(employee);
-     }
-
-     @GetMapping("/employee/get")
-     public List<Employee> getEmployee() {
-         return  employeeService.findEmployees();
-      }
+    @GetMapping("/get")
+    public List<Employee> getEmployees() {
+        return employeeService.findEmployees();
+    }
 }
